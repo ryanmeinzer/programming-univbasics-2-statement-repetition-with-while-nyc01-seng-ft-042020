aa7636ffd21f615b25b00677257d0de3eda53e24
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-01 01:04:20 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,15 @@
 # Write your code here
 
+# count = 10
+# while count >= 1 do
+#   puts count
+#   count -= 1
+# end
+# 
+# puts "Happy New Year!"
+
 count = 10
-while count >= 1 do
+10.times do
   puts count
   count -= 1
 end
